88d164780554a98f4aad73d14a301cacfc93e1cd
Gabriel Orozco <j_gabrielorozco@live.com>
2023-12-16 09:52:16 -0500

    comments added

--- pom/login_page.py
+++ pom/login_page.py
@@ -13,6 +13,7 @@ class LoginPage:
         self.browser_interactions = browser_interactions
         self.cons = Constants
 
+    # Returns the entire expected message from its name
     def _get_message(self, msg_name: str):
         msgs = {
             "correct login": self.cons.login_message,
@@ -21,31 +22,40 @@ class LoginPage:
         }
         return msgs[msg_name]
 
+    # Enters user and password, both correct and from .env file
     def enter_user_pass(self, username: str, password: str):
         if self.browser_interactions.input_text(locators.USERNAME_FIELD, username):
             if self.browser_interactions.input_text(locators.PASSWORD_FIELD, password):
                 return True
         return False
 
+    # Redirects to the login page
     def to_login_page(self, url: str):
         self.browser_interactions.open_page(url)
 
+    # Clicks on login button
     def click_login(self):
         self.browser_interactions.click_element(locators.LOGIN_BUTTON)
 
+    # Check if the messages are the expected
     def check_login_message (self, msg_name: str):
         login_message = self.browser_interactions.get_element(locators.LOGIN_MESSAGE).text
         return login_message.__contains__(self._get_message(msg_name))
 
+    # Check if login is correct. Check if the logout button appears
     def check_login(self):
         return self.browser_interactions.element_is_visible(locators.LOGOUT_BUTTON)
 
+    # Enters a username in the username field. It can be the correct one (from .env) or an invalid one
+    # defined in a test case
     def enter_username(self, which="correct", username=" "):
         if which == "correct":
             self.browser_interactions.input_text(locators.USERNAME_FIELD, os.getenv("USERNAME"))
         elif which == "invalid":
             self.browser_interactions.input_text(locators.USERNAME_FIELD, username)
 
+    # Enters a password in the password field. It can be the correct one (from .env) or an invalid one
+    # defined in a test case
     def enter_password(self, which="correct", password=" "):
         if which == "correct":
             self.browser_interactions.input_text(locators.PASSWORD_FIELD, os.getenv("PASSWORD"))
