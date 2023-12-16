db3fc528ff19e5c4bd74e3f4ac5ca13254e5dccb
Gabriel Orozco <j_gabrielorozco@live.com>
2023-12-16 11:14:38 -0500

    comments added

--- pom/upload_file_page.py
+++ pom/upload_file_page.py
@@ -6,15 +6,19 @@ class UploadFilePage:
     def __init__(self, browser_interactions: BrowserInteractions):
         self.browser_interactions = browser_interactions
 
+    # Redirects to upload file page
     def to_upload_page(self, url):
         self.browser_interactions.open_page(url)
 
+    # Attach a file
     def attach_file(self, file_root: str):
         return self.browser_interactions.input_text(locators.FILE_FIELD, file_root)
 
+    # Upload the file using upload button
     def upload_file(self):
         self.browser_interactions.click_element(locators.UPLOAD_BUTTON)
 
+    # Check if the file was uploaded checking the message displayed after the process is done
     def check_upload(self):
         msg = self.browser_interactions.get_element(locators.UPLOADED_MSG).text
         return msg.__contains__(cons.FILE_UPLOADED)
